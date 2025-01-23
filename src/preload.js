d3c72f83d7255e7eb6a99288917b7b205cb37604
stagnet <brucewayne128gb@protonmail.com>
2025-01-23 09:09:30 +0530

    minor changes

--- src/preload.js
+++ src/preload.js
@@ -1,6 +1,6 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-
+// preload.js
 const { contextBridge, ipcRenderer } = require('electron');
 console.log('preload.js');
 
